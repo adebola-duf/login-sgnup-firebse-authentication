abb91c3c9c4e8bad859bb5441ff8452fb00661cc
adeboladuf <adeboladuf@gmail.com>
2024-05-15 12:48:18 +0000

    Innitial comit - login page for mobile.

---
+++ lib/login_page.dart
@@ -0,0 +1,280 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Opacity(
+            opacity: .87,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/fidelity-lady.png',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              height: double.infinity,
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                child: Container(
+                  decoration:
+                      BoxDecoration(color: Colors.white.withOpacity(0.0)),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 30),
+                child: Column(
+                  children: [
+                    Text(
+                      'Welcome',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.smoochSans(
+                        textStyle: const TextStyle(
+                          height: .7,
+                          fontSize: 110,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const Gap(10),
+                    Text(
+                      'We are glad to see you back with us.',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const Gap(29),
+                    TextFieldContainer(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Username',
+                          hintStyle: GoogleFonts.poppins(
+                            textStyle: const TextStyle(fontSize: 12),
+                          ),
+                          prefixIcon: const Icon(Icons.person_2_outlined),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const Gap(24),
+                    TextFieldContainer(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: GoogleFonts.poppins(
+                            textStyle: const TextStyle(fontSize: 12),
+                          ),
+                          prefixIcon: const Icon(Icons.lock),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const Gap(24),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 52,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(13),
+                          ),
+                          foregroundColor: Colors.white,
+                          backgroundColor:
+                              const Color.fromARGB(255, 28, 28, 28),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'NEXT',
+                          style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Gap(24),
+                    RichText(
+                      text: TextSpan(
+                        style: GoogleFonts.poppins(
+                            textStyle: const TextStyle(fontSize: 16)),
+                        children: const <TextSpan>[
+                          TextSpan(
+                            text: 'Login ',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'with others',
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Gap(24),
+                    MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          height: 52,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(13),
+                            border: Border.all(color: Colors.black, width: 1),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SvgPicture.asset(
+                                'assets/icons/google-logo.svg',
+                                height: 30,
+                              ),
+                              const Gap(6),
+                              RichText(
+                                text: TextSpan(
+                                  style: GoogleFonts.poppins(
+                                      textStyle: const TextStyle(fontSize: 12)),
+                                  children: const <TextSpan>[
+                                    TextSpan(
+                                      text: 'Login with',
+                                    ),
+                                    TextSpan(
+                                      text: ' google',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Gap(16),
+                    MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          height: 52,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            borderRadius: BorderRadius.circular(13),
+                            border: Border.all(color: Colors.black, width: 1),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SvgPicture.asset(
+                                'assets/icons/microsoft-logo.svg',
+                                height: 30,
+                              ),
+                              const Gap(6),
+                              RichText(
+                                text: TextSpan(
+                                  style: GoogleFonts.poppins(
+                                      textStyle: const TextStyle(fontSize: 12)),
+                                  children: const <TextSpan>[
+                                    TextSpan(
+                                      text: 'Login with',
+                                    ),
+                                    TextSpan(
+                                      text: ' google',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Gap(18),
+                    MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          'Don\'t have an account? Sign up here',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Gap(14),
+                    MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TextFieldContainer extends StatelessWidget {
+  const TextFieldContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 52,
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 242, 242, 242),
+        borderRadius: BorderRadius.circular(13),
+      ),
+      child: Center(child: child),
+    );
+  }
+}
